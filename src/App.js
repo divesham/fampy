93b61897d3c0bd819b6e785deca28b8682ef918c
divesham <41263730+divesham@users.noreply.github.com>
2020-09-29 19:15:02 +0530

    intital React Set Up

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SpinnerApp from './components/SpinnerApp';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+         <SpinnerApp/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
